30373b9547f7260bf99118c3e70973624f679a58
mikaela.pisani <mikaela.pisani@rootstrap.com>
2021-01-08 16:07:12 -0300

    add observations

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -18,11 +18,11 @@
              code: {
                $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                      'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                     'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                     'http://loinc.org|2089-1', 'http://loinc.org|55284-4', 
+                     'http://loinc.org|26478-8', 'http://loinc.org|2345-7']
              }
            }
          });
-
 
         console.log('patient:');
         console.log(patient)
@@ -65,16 +65,16 @@
           p.lymph = getQuantityValueAndUnit(lymph[0]);
 
 
-        // Cerner SoF Tutorial Observations
-         p.height = getQuantityValueAndUnit(height[0]);
+          // Cerner SoF Tutorial Observations
+          p.height = getQuantityValueAndUnit(height[0]);
 
-         if (typeof systolicbp != 'undefined')  {
-           p.systolicbp = systolicbp;
-         }
+          if (typeof systolicbp != 'undefined')  {
+            p.systolicbp = systolicbp;
+          }
 
-         if (typeof diastolicbp != 'undefined') {
-           p.diastolicbp = diastolicbp;
-         }
+          if (typeof diastolicbp != 'undefined') {
+            p.diastolicbp = diastolicbp;
+          }
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
@@ -101,11 +101,11 @@
       lymph: {value: ''}
 
       // Cerner SoF Tutorial Observations
-       height: {value: ''},
-       systolicbp: {value: ''},
-       diastolicbp: {value: ''},
-       ldl: {value: ''},
-       hdl: {value: ''},
+      height: {value: ''},
+      systolicbp: {value: ''},
+      diastolicbp: {value: ''},
+      ldl: {value: ''},
+      hdl: {value: ''},
     };
   }
 
@@ -150,11 +150,11 @@
     
     // Cerner SoF Tutorial Observations
 
-     $('#height').html(p.height);
-     $('#systolicbp').html(p.systolicbp);
-     $('#diastolicbp').html(p.diastolicbp);
-     $('#ldl').html(p.ldl);
-     $('#hdl').html(p.hdl);
+    $('#height').html(p.height);
+    $('#systolicbp').html(p.systolicbp);
+    $('#diastolicbp').html(p.diastolicbp);
+    $('#ldl').html(p.ldl);
+    $('#hdl').html(p.hdl);
   };
 
 })(window);
