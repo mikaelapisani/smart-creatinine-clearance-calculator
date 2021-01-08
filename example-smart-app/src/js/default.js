7b85e1df07612b9f178005c25056d3c64a887189
mikaela.pisani <mikaela.pisani@rootstrap.com>
2021-01-08 16:51:25 -0300

    add observations

---
+++ example-smart-app/src/js/default.js
@@ -0,0 +1,16 @@
+function defaultPatient(){
+    return {
+      fname: {value: ''},
+      lname: {value: ''},
+      gender: {value: ''},
+      birthdate: {value: ''},
+      lymph: {value: ''},
+
+      // Cerner SoF Tutorial Observations
+      height: {value: ''},
+      systolicbp: {value: ''},
+      diastolicbp: {value: ''},
+      ldl: {value: ''},
+      hdl: {value: ''}
+    };
+  }
